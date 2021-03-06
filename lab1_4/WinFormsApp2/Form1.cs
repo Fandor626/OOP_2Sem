9d58494bf6e3e50df1b9c8ffb70cab69ead3fb65
Fandor626 <58294921+Fandor626@users.noreply.github.com>
2021-03-06 12:44:39 +0200

    Add files via upload

---
+++ lab1_4/WinFormsApp2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1_4
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Text = "Label" + count.ToString();
+            label.Font = new Font("Arial", 10, FontStyle.Bold);
+            label.Location = new Point(10, 50 * count);
+            this.Controls.Add(label);
+            count++;
+        }
+    }
+}
